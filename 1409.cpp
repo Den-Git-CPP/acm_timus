6faf0630b03ea998f1ee9acdd4740a6dc999a97e
Den-Git-CPP <madim-neodart@rambler.ru>
2022-05-05 23:34:12 +0300

    Дз 1409

---
+++ 1409.cpp
@@ -0,0 +1,12 @@
+﻿// 1409.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+int main( ) 
+{
+	int a, b;
+	std::cin >> a >> b;
+	int s = a + b - 1;
+	std::cout << s - a << " " << s - b << std::endl;
+	return 0;
+}
